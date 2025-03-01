7c7562be8f015c681fd64df565d0679eb8be657e
heynzar <heynzar@gmail.com>
2025-03-01 12:38:18 +0000

    feat: update AppSidebar component to enhance navigation with new goals and tasks sections

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -3,20 +3,15 @@
 import * as React from "react";
 import {
   BellDot,
-  Calendar,
-  CalendarDays,
-  Inbox,
-  LayoutGrid,
-  Search,
+  Goal,
+  CircleCheckBig,
+  Pentagon,
+  Clock9,
   SwatchBook,
 } from "lucide-react";
 
 import { NavMain } from "@/components/sidebar/nav-main";
 import { NavWorkspaces } from "@/components/sidebar/nav-workspaces";
-// import { NavFavorites } from "@/components/nav-favorites";
-// import { NavSecondary } from "@/components/nav-secondary";
-// import { TeamSwitcher } from "@/components/team-switcher";
-// import { NavActions } from "./nav-actions";
 import {
   Sidebar,
   SidebarContent,
@@ -24,137 +19,51 @@ import {
   SidebarHeader,
   SidebarMenuButton,
   SidebarRail,
+  SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { Button } from "../ui/button";
-import Link from "next/link";
+
 import { ProfileActions } from "@/components/sidebar/profile-actions";
 
 const data = {
   navMain: [
     {
-      title: "Search",
-      url: "#",
-      icon: Search,
-    },
-    {
-      title: "Inbox",
-      url: "inbox",
-      icon: Inbox,
-    },
-    {
-      title: "Today",
-      url: "today",
-      icon: Calendar,
+      title: "Home",
+      url: "/",
+      icon: Pentagon,
       isActive: true,
     },
     {
-      title: "Upcoming",
-      url: "upcoming",
-      icon: CalendarDays,
-      badge: "10",
+      title: "Goals",
+      url: "goals",
+      icon: Goal,
     },
     {
-      title: "Filters & Labels",
-      url: "filters-labels",
-      icon: LayoutGrid,
+      title: "Tasks",
+      url: "tasks",
+      icon: CircleCheckBig,
+    },
+    {
+      title: "Pomodoros",
+      url: "pomodoros",
+      icon: Clock9,
     },
   ],
 
   workspaces: [
     {
-      name: "Personal Life Management",
-      emoji: "🏠",
-      pages: [
-        {
-          name: "Daily Journal & Reflection",
-          url: "#",
-          emoji: "📔",
-        },
-      ],
+      name: "Career Objectives & Milestones",
+      url: "#",
+      emoji: "🎯",
     },
     {
-      name: "Professional Development",
-      emoji: "💼",
-      pages: [
-        {
-          name: "Career Objectives & Milestones",
-          url: "#",
-          emoji: "🎯",
-        },
-        {
-          name: "Skill Acquisition & Training Log",
-          url: "#",
-          emoji: "🧠",
-        },
-        {
-          name: "Networking Contacts & Events",
-          url: "#",
-          emoji: "🤝",
-        },
-      ],
+      name: "Skill Acquisition & Training Log",
+      url: "#",
+      emoji: "🧠",
     },
     {
-      name: "Creative Projects",
-      emoji: "🎨",
-      pages: [
-        {
-          name: "Writing Ideas & Story Outlines",
-          url: "#",
-          emoji: "✍️",
-        },
-        {
-          name: "Art & Design Portfolio",
-          url: "#",
-          emoji: "🖼️",
-        },
-        {
-          name: "Music Composition & Practice Log",
-          url: "#",
-          emoji: "🎵",
-        },
-      ],
-    },
-    {
-      name: "Home Management",
-      emoji: "🏡",
-      pages: [
-        {
-          name: "Household Budget & Expense Tracking",
-          url: "#",
-          emoji: "💰",
-        },
-        {
-          name: "Home Maintenance Schedule & Tasks",
-          url: "#",
-          emoji: "🔧",
-        },
-        {
-          name: "Family Calendar & Event Planning",
-          url: "#",
-          emoji: "📅",
-        },
-      ],
-    },
-    {
-      name: "Travel & Adventure",
-      emoji: "🧳",
-      pages: [
-        {
-          name: "Trip Planning & Itineraries",
-          url: "#",
-          emoji: "🗺️",
-        },
-        {
-          name: "Travel Bucket List & Inspiration",
-          url: "#",
-          emoji: "🌎",
-        },
-        {
-          name: "Travel Journal & Photo Gallery",
-          url: "#",
-          emoji: "📸",
-        },
-      ],
+      name: "Networking Contacts & Events",
+      url: "#",
+      emoji: "🤝",
     },
   ],
 };
@@ -165,23 +74,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader>
         <div className="flex justify-between gap-2 items-center mt-1.5">
           <ProfileActions />
-          <Link href={"/notifications"}>
-            <Button variant="ghost" className="size-7">
-              <BellDot />
-            </Button>
-          </Link>
+          <SidebarTrigger />
         </div>
         <NavMain items={data.navMain} />
       </SidebarHeader>
       <SidebarContent>
         <NavWorkspaces workspaces={data.workspaces} />
       </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenuButton className="text-sidebar-foreground">
-          <SwatchBook />
-          <span>Browse templates</span>
-        </SidebarMenuButton>
-      </SidebarFooter>
+
       <SidebarRail />
     </Sidebar>
   );

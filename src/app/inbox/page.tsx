9a4a9d3589ebdebf7da74f94cb337f3e066a8430
heynzar <heynzar@gmail.com>
2025-03-04 13:42:52 +0000

    feat: integrate AddTaskDialog into inbox page for task management and display tasks dynamically

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,19 +1,30 @@
+"use client";
+import { AddTaskDialog } from "@/components/sidebar/add-task/add-task-dialog";
 import TaskCard from "@/components/task/task";
 import { Plus } from "lucide-react";
-
+import { useTasks } from "@/context/TasksContext";
 function pape() {
+  const { tasks, addTask } = useTasks();
+
   return (
     <main className="px-32">
       <h2 className="font-bold text-2xl">Inbox</h2>
       <section className="py-6">
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
+        {tasks.map(({ id, task, description, isCompleted }) => (
+          <TaskCard
+            key={id}
+            task={task}
+            description={description}
+            isCompleted={isCompleted}
+          />
+        ))}
 
-        <button className="w-full py-2 flex items-center gap-2 text-sm text-muted-foreground hover:text-red-400">
-          <Plus className="size-5" strokeWidth={1} />
-          <span className="">Add Task</span>
-        </button>
+        <AddTaskDialog addTask={addTask}>
+          <button className="w-full py-2 flex items-center gap-2 text-sm text-muted-foreground hover:text-red-400">
+            <Plus className="size-5" strokeWidth={1} />
+            <span className="">Add Task</span>
+          </button>
+        </AddTaskDialog>
       </section>
     </main>
   );
